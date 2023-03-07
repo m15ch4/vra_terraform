603e682fc56c4cceb2f95cc3d1a728b38bb34ea1
Michal Czerwinski <m15ch4@gmail.com>
2023-03-07 16:40:36 +0100

    modified variables

--- Azure-s2s/variables.tf
+++ Azure-s2s/variables.tf
@@ -1,3 +1,15 @@
+variable "rg-name" {
+  description = "Resource Group Name"
+  type        = string
+  default = "rg1"
+}
+
+variable "rg-location" {
+  description = "Resource Group Location"
+  type        = string
+  default = "westus2"
+}
+
 variable "vnet-name" {
   description = "Virtual Network"
   type        = string
